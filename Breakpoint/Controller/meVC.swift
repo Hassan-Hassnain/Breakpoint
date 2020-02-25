9262710841089444244529d62419f0e40001a2b4
Hassan Hassnain <ahh.devoders@gmail.com>
2020-02-26 01:36:40 +0500

    Lecture 10 completed

---
+++ Breakpoint/Controller/meVC.swift
@@ -0,0 +1,28 @@
+//
+//  meVC.swift
+//  Breakpoint
+//
+//  Created by Usama Sadiq on 2/26/20.
+//  Copyright © 2020 Usama Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class meVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func signOutButtonWasPressed(_ sender: Any) {
+    }
+    
+
+}
